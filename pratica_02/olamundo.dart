0b0eadbebe6905d5b0bd04e3e77a99f2e4ba9f21
guilhermejcmarinho <gui.the.great@gmail.com>
2021-10-22 18:13:31 -0300

    praticas iniciais

---
+++ pratica_02/olamundo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const Text(
+      'Ola, mundo!',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
---
+++ pratica_02/pratica_02.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        "Guilherme Marinho!",
+        textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
